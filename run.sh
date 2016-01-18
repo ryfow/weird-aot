b1ee8cbc815e0c86c608b73bb29f276c73e50f97
Ryan Fowler <ryan.fowler@singlewire.com>
2016-01-18 16:45:34 -0600

    update run.sh to compile java code

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 CP=$(lein classpath)
 echo "Classpath: " $CP
+rm -rf target/classes
+mkdir -p target/classes
+javac -cp $CP -d target/classes -sourcepath java java/weird_aot/AClass.java
 java -cp $CP clojure.main compile.clj 
 
